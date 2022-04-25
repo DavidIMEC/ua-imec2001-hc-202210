c3d4a0c08f89cab7fcf964dbdca18128e1d8ffb0
DavidIMEC <103160927+DavidIMEC@users.noreply.github.com>
2022-04-24 20:06:23 -0500

    Create Pruebas.jl

---
+++ content/week2/Pruebas.jl
@@ -0,0 +1,11 @@
+using DataFrames, XLSX, CSV
+
+print("a")
+#min_df = DataFrame(XLSX.readtable("./data/SD_5Min.xlsx", "Potencia_AC", header=true)...)
+#min_cop = copy(min_df)
+
+#sol_df = DataFrame(CSV.File("./data/solar_production.csv", header = 1, delim = ";")...)
+#sol_cop = copy(min_df)
+
+#describe(min_df)
+#describe(sol_df)
